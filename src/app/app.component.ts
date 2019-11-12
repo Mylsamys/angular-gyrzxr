e57dfc8c54546f75063a2251f415e9a0f4705a92
Mylsamys <samy.myls@gmail.com>
2019-11-12 16:05:47 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl,Validators } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './Binding.html',
